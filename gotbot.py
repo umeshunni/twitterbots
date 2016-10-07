649dc9058742b95914383182ee30bcd39c2523e5
Umesh Unnikrishnan <umeshunni@umeshunni.com>
2016-10-07 10:46:33 -0700

    modified:   gotbot.py

--- gotbot.py
+++ gotbot.py
@@ -1,12 +1,9 @@
 import praw
 import re
 import tweepy
+import secrets
 
 
-consumer_key = "af3v5r8FhE2rGXNEt1Q3vEGGi"
-consumer_secret = "BQWE3Hh30VX1LptNn9k6z0YbCu0bYCTx6xpvn6rxcuQQdDd1xk"
-access_token = "2492242001-AhQ3EoEvgzRukljOZtfKZDckyhrL7nkXhxxWEti"
-access_token_secret = "UVYp4x4FIxJMNRp8RlBCvjldKwmBz48mSaI40vQT5IFwq"
 
 def cleantitle(title):
 	return re.findall("^\[.*\](.*)", title)[0]
@@ -20,12 +17,15 @@ def parsepost(post):
 
 
 def main():	
+	
+	secrets.init()
+
 	reddit = praw.Reddit(user_agent="twitter.com:tweets_iaf v 0.0.1 by /u/umeshunni")
 	
 	hotposts = reddit.get_subreddit('gameofthrones').get_top(limit=10)
 
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_token, access_token_secret)
+	auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
+	auth.set_access_token(secrets.access_token, secrets.access_token_secret)
 
 	twitter = tweepy.API(auth)
 
@@ -35,7 +35,7 @@ def main():
 			print "Skipping: " + status 
 		else:
 			print "Tweeting: " + status 
-			twitter.update_status(status)
+			#twitter.update_status(status)
 	
 
 
